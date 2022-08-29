518d63da660f022ba7adc31827a18b64919ad5e3
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-29 14:35:51 +0300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { ModuleFilenameHelpers } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+          filename: 'index.html',
+        }),
+      ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
+}
